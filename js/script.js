9f2ee8e86cc976769e912e58ba6638d16b173f70
Yi-Steven-Zhou <yi.zhou@stu.ocsb.ca>
2025-05-21 00:10:28 +0000

    create cookieclicker

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emre Guzel
+// Created on: May 14 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+
+// Setting the cookie number
+let cookieNumber = 0;
+
+
+// Setting the cookieClickedBtn function
+function cookieClickedBtn() {
+  // Setting the variables 
+
+
+  //Increase cookie number
+  cookieNumber ++
+  // // Save the updated value back to localStorage
+  localStorage.setItem("cookieNumber",cookieNumber)
+  showCookieNumber()
+
+}
+function showCookieNumber(){
+  let cookieNumberFromStorge = localStorage.getItem("cookieNumber")
+
+  // check local stoge has a value
+  if (cookieNumberFromStorge != null) {
+    cookieNumber = parseInt(cookieNumberFromStorge)
+  }
+  // Display result
+  document.getElementById("result").innerHTML =
+    "You have clicked " + (cookieNumber) + " times.";
+}
